0a2b0f4c082faea9eee1140332d9e5d2a037518d
Mark Ryall <mark@ryall.name>
2012-07-21 22:38:59 +0800

    added several new phrases

--- lib/flash/corpus.rb
+++ lib/flash/corpus.rb
@@ -6,6 +6,18 @@ module Flash
     Zàijiàn,再见,Goodbye
     Bùyòng xiè,不用谢,You're welcome (more familar)
     Bù kèqì,不客气,You're welcome (more polite)
+    Xièxiè,谢谢,Thank you
+    Mǎ,马,Horse
+    Kè,克,Gram
+    Mā,妈,Mum
+    Wǒ bù huì jiǎng zhōngwén,我不会讲中文,I do not speak Chinese
+    Zǎoshang hǎo,早上好, Good morning
+    Zhōngwǔ hǎo,中午好,Good afternoon
+    Wǎnshàng hǎo,晚上好,Good evening
+    Wǎn'ān,晚安,Good night
+    Duōshǎo qián,多少钱,How much money
+    Zhège,这个,This
+    Qīn'ài de,亲爱的,Darling
     EOF
 
     def phrase
