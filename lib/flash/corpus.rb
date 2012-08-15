e473a0145474c8a137535f1a0b33e5233e47c7c9
Mark Ryall <mark@ryall.name>
2012-08-15 22:33:48 +0800

    changed file format to make it a little more readable and flexible

--- lib/flash/corpus.rb
+++ lib/flash/corpus.rb
@@ -1,14 +1,25 @@
-# coding: utf-8
 module Flash
   class Corpus
-    attr_accessor :phrases
-
-    def initialize path
-      self.phrases = File.read(path).split("\n").map {|line| line.chomp.strip.split "," }.select {|l| l.size > 1}
+    def initialize *paths
+      @phrases = []
+      paths.each do |path|
+        File.open path do |file|
+          entry = []
+          file.each_line do |line|
+            if line.chomp.empty?
+              @phrases << entry unless entry.empty?
+              entry = []
+            else
+              entry << line.chomp.strip
+            end
+          end
+          @phrases << entry unless entry.empty?
+        end
+      end
     end
 
     def phrase
-      phrases[rand(phrases.count)].tap do |p|
+      @phrases[rand(@phrases.count)].tap do |p|
         p.reverse! if flip?
       end
     end
